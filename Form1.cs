935643ba7620015f134e7cfa4c9b6f29d008ff4a
Nicholas Regehr <Nicholas.Regehr@infotech.pri>
2021-09-10 10:52:59 -0600

    create

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaClassDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Pizza p = new Pizza("small", "pepporoni", "pepperoni");
+            MessageBox.Show(p.Cost.ToString());
+
+            Pizza p2 = new Pizza("large", "hawaiian", "pineapple", "ham", "bacon");
+            MessageBox.Show(p2.Cost.ToString());
+        }
+
+    }
+}
---
+++ Pizza.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaClassDemo
+{
+
+    //access modifier is private by default
+    public class Pizza
+    {
+        public Pizza(string _size, string _name, params string[] toppings)
+        {
+            this._size = _size;
+            this._name = _name;
+            this._toppings.AddRange(toppings);
+        }
+        //Fields
+        private string _size;
+        private string _name;
+        private List<string> _toppings = new List<string>();
+
+        //Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Size
+        {
+            get { return _size; }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                decimal tmp = 0;
+                //Assign cost based on size
+                if (_size == "small")
+                    tmp += 4.99m;
+                if (_size == "medium")
+                    tmp += 7.99m;
+                if (_size == "large")
+                    tmp += 9.99m;
+                //Charge $1.50 per topping
+                tmp += _toppings.Count * 1.5m;
+
+                return tmp;
+            }
+        }
+    }
+}
